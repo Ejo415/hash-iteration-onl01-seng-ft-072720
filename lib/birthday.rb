688eadfd25bf43b4283bf5275088d1d2fe65fd7c
Eugene O'Neil <ejo7220@gmail.com>
2020-07-31 17:52:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,8 @@ end
 
 def age_approprate_birthday(birthday_kids)
 birthday_kids.each do |name, age|
-  if age < 12 puts 
+  if age < 12 puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else 
+end
 end
 
